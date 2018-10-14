b7c4eeb32f4dfea1a6aa98e5e53fef65768c0433
uufish <uufish.io@gmail.com>
2018-10-15 04:44:22 +0700

    add: ChangelogExpansionPanel

---
+++ src/containers/ChangelogExpansionPanel.js
@@ -0,0 +1,44 @@
+import ExpansionPanel from '@material-ui/core/ExpansionPanel'
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails/ExpansionPanelDetails'
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary'
+import withStyles from '@material-ui/core/styles/withStyles'
+import Typography from '@material-ui/core/Typography/Typography'
+import ExpandMore from '@material-ui/icons/ExpandMore'
+import React, { Component } from 'react'
+
+class ChangelogExpansionPanel extends Component<any, any> {
+  state = {}
+
+  render() {
+    const { classes, version, description, date } = this.props
+
+    return (
+      <ExpansionPanel>
+        <ExpansionPanelSummary expandIcon={<ExpandMore />}>
+          <Typography className={classes.version}>{version}</Typography>
+          <Typography className={classes.date}>{date}</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Typography className={classes.description}>{description}</Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+    )
+  }
+}
+
+const styles = () => ({
+  version: {
+    flexBasis: '33.33%',
+    flexShrink: 0
+  },
+  date: {
+    opacity: 0.65,
+    fontSize: 12
+  },
+  description: {
+    whiteSpace: 'pre-line',
+    wordBreak: 'break-alls'
+  }
+})
+
+export default withStyles(styles)(ChangelogExpansionPanel)
