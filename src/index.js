25e9069dfec6b78aace4b0309bee192abb33f433
uufish <uufish.io@gmail.com>
2018-10-14 17:55:25 +0700

    update

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
+import './libs/initializeApp'
 import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(<App />, document.getElementById('root'))
