f1bfb035ba320edc2d98de821ba2b5188d464755
uufish <uufish.io@gmail.com>
2018-10-14 23:01:34 +0700

    add: pages

---
+++ src/pages/PageDevelopment.js
@@ -0,0 +1,25 @@
+import withStyles from '@material-ui/core/styles/withStyles'
+import Typography from '@material-ui/core/Typography/Typography'
+import React, { Component } from 'react'
+
+class PageDevelopment extends Component<any, any> {
+  render() {
+    const { classes } = this.props
+
+    return (
+      <div className={classes.root}>
+        <Typography align={'center'}>
+          このページはまだ利用できません。
+        </Typography>
+      </div>
+    )
+  }
+}
+
+const styles = () => ({
+  root: {
+    paddingTop: 160
+  }
+})
+
+export default withStyles(styles)(PageDevelopment)
