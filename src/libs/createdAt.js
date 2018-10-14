60f689de845ea7589dc6dd82fd498f83aeedc686
uufish <uufish.io@gmail.com>
2018-10-15 01:58:16 +0700

    fix

--- src/libs/createdAt.js
+++ src/libs/createdAt.js
@@ -3,7 +3,7 @@
 export const createdAt = (seconds: number) => {
   const date = new Date(seconds * 1000)
   const Y = date.getFullYear()
-  const M = date.getMonth() - 1
+  const M = date.getMonth() + 1
   const D = date.getDate()
   const h = date.getHours()
   const m = date.getMinutes()
