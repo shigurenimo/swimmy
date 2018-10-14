923581e1bdbaaa34ef937998ab26d89c22c677d1
uufish <uufish.io@gmail.com>
2018-10-14 17:56:40 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-import React, { Component } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider'
+import withStyles from '@material-ui/core/styles/withStyles'
+import React, { Component, Fragment } from 'react'
+import AppRouter from './AppRouter'
+import AppAuthProvider from './containers/AppAuthProvider'
+import { theme } from './theme'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <AppAuthProvider>
+          <Fragment>
+            <CssBaseline />
+            <AppRouter />
+          </Fragment>
+        </AppAuthProvider>
+      </MuiThemeProvider>
     )
   }
 }
 
-export default App
+const styles = () => ({})
+
+export default withStyles(styles)(App)
