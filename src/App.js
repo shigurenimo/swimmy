f4b3aaffeab5970dcb1245dc4064fe9407d02899
uufish <uufish.io@gmail.com>
2018-10-15 00:38:00 +0700

    fix: JssProvider

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import CssBaseline from '@material-ui/core/CssBaseline'
+import { createGenerateClassName } from '@material-ui/core/styles'
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider'
 import withStyles from '@material-ui/core/styles/withStyles'
 import React, { Component, Fragment } from 'react'
+import JssProvider from 'react-jss/lib/JssProvider'
 import AppRouter from './AppRouter'
 import AppAuthProvider from './containers/AppAuthProvider'
 import { theme } from './theme'
 
+const generateClassName = createGenerateClassName({
+  dangerouslyUseGlobalCSS: false,
+  productionPrefix: 'c'
+})
+
 class App extends Component {
   render() {
     return (
-      <MuiThemeProvider theme={theme}>
-        <AppAuthProvider>
-          <Fragment>
-            <CssBaseline />
-            <AppRouter />
-          </Fragment>
-        </AppAuthProvider>
-      </MuiThemeProvider>
+      <JssProvider generateClassName={generateClassName}>
+        <MuiThemeProvider theme={theme}>
+          <AppAuthProvider>
+            <Fragment>
+              <CssBaseline />
+              <AppRouter />
+            </Fragment>
+          </AppAuthProvider>
+        </MuiThemeProvider>
+      </JssProvider>
     )
   }
 }
