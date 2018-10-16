1025e7509f519a367fc62a4f396e997c24fe2963
uufish <uufish.io@gmail.com>
2018-10-17 04:18:27 +0700

    update

--- src/components/PostActions.js
+++ src/components/PostActions.js
@@ -1,26 +1,25 @@
 import IconButton from '@material-ui/core/IconButton/IconButton'
 import withStyles from '@material-ui/core/styles/withStyles'
-import Launch from '@material-ui/icons/Launch'
 import Delete from '@material-ui/icons/Delete'
 import Favorite from '@material-ui/icons/Favorite'
+import Launch from '@material-ui/icons/Launch'
 import React, { Component } from 'react'
 
 class PostActions extends Component {
-  isUnmounted = false
-
-  state = {
-    inProgress: false
-  }
-
   render() {
-    const { classes } = this.props
+    const { classes, inProgressLike, hasLike } = this.props
 
     return (
       <div className={classes.root}>
         <IconButton className={classes.iconButton} disabled={true}>
           <Delete />
         </IconButton>
-        <IconButton className={classes.iconButton} disabled={true}>
+        <IconButton
+          className={classes.iconButton}
+          onClick={this.onClickLike}
+          disabled={inProgressLike}
+          color={hasLike ? 'secondary' : 'default'}
+        >
           <Favorite />
         </IconButton>
         <IconButton className={classes.iconButton} onClick={this.onSelectPost}>
@@ -30,14 +29,16 @@ class PostActions extends Component {
     )
   }
 
+  onClickLike = () => {
+    const { postId, onClickLike } = this.props
+
+    onClickLike(postId)
+  }
+
   onSelectPost = () => {
     const { postId, selectPost } = this.props
 
     selectPost(postId)
-  }
-
-  componentWillUnmount() {
-    this.isUnmounted = true
   }
 }
 
--- src/containers/PostExpansionPanel.js
+++ src/containers/PostExpansionPanel.js
@@ -1,30 +1,43 @@
 import ExpansionPanel from '@material-ui/core/ExpansionPanel'
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary'
 import withStyles from '@material-ui/core/styles/withStyles'
+import { auth, firestore } from 'firebase/app'
 import React, { Component } from 'react'
 import PostActions from '../components/PostActions'
 import PostAsReplyList from '../components/PostAsReplyList'
 import PostAsReplyTextField from '../components/PostAsReplyTextField'
 import PostExpansionPanelSummary from '../components/PostExpansionPanelSummary'
+import { LIKES, POSTS } from '../constants/collection'
+import { createPostLike } from '../libs/createPostLike'
 
 class PostExpansionPanel extends Component<any, any> {
+  isUnmounted = false
+
   state = {
-    expanded: false
+    expanded: false,
+    hasLike: false,
+    inProgressLike: true
   }
 
   render() {
     const { classes, post, selectPost } = this.props
-    const { expanded } = this.state
+    const { expanded, inProgressLike, hasLike } = this.state
 
     return (
-      <ExpansionPanel expanded={expanded}>
+      <ExpansionPanel expanded={expanded} onChange={this.onChangeExpand}>
         <ExpansionPanelSummary
           className={classes.summary}
           onClick={this.onClickPanelSummary}
         >
           <PostExpansionPanelSummary post={post} />
         </ExpansionPanelSummary>
-        <PostActions selectPost={selectPost} postId={post.id} />
+        <PostActions
+          selectPost={selectPost}
+          onClickLike={this.clickLike}
+          postId={post.id}
+          inProgressLike={inProgressLike}
+          hasLike={hasLike}
+        />
         {expanded && (
           <PostAsReplyList
             postId={post.id}
@@ -36,6 +49,53 @@ class PostExpansionPanel extends Component<any, any> {
         </div>
       </ExpansionPanel>
     )
+  }
+
+  componentDidMount() {}
+
+  componentWillUnmount() {
+    this.isUnmounted = true
+  }
+
+  clickLike = (postId: string) => {
+    if (this.isUnmounted) return
+
+    const { inProgress } = this.props
+
+    if (inProgress) return
+
+    this.setState(state => ({ hasLike: !state.hasLike }))
+
+    createPostLike({ postId }).catch(err => {
+      console.error(err)
+    })
+  }
+
+  onChangeExpand = (_, expanded) => {
+    if (this.isUnmounted) return
+
+    const { post } = this.props
+
+    if (!expanded) return
+
+    const { inProgressLike } = this.state
+
+    if (!inProgressLike) return
+
+    const { currentUser } = auth()
+
+    if (!currentUser) return
+
+    firestore()
+      .collection(LIKES)
+      .where('collectionId', '==', POSTS)
+      .where('ownerId', '==', currentUser.uid)
+      .where('docId', '==', post.id)
+      .get()
+      .then(res => {
+        if (this.isUnmounted) return
+        this.setState({ inProgressLike: false, hasLike: !res.empty })
+      })
   }
 
   onClickPanelSummary = event => {
