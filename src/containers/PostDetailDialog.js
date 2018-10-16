856e8d2d6711f073e085b86e21b1200f2761cfba
uufish <uufish.io@gmail.com>
2018-10-17 03:29:05 +0700

    add: containers/PostDetailDialog

---
+++ src/containers/PostDetailDialog.js
@@ -0,0 +1,44 @@
+import Button from '@material-ui/core/Button/Button'
+import CircularProgress from '@material-ui/core/CircularProgress/CircularProgress'
+import Dialog from '@material-ui/core/Dialog/Dialog'
+import DialogActions from '@material-ui/core/DialogActions/DialogActions'
+import DialogContent from '@material-ui/core/DialogContent/DialogContent'
+import List from '@material-ui/core/List/List'
+import withStyles from '@material-ui/core/styles/withStyles'
+import React, { Component } from 'react'
+import ReplyPostlistItem from '../components/ReplyPostlistItem'
+
+class PostDetailDialog extends Component<any, any> {
+  render() {
+    const { onClose, isOpen, posts, inProgress, classes } = this.props
+
+    return (
+      <Dialog fullScreen open={isOpen} onClose={onClose}>
+        <DialogActions>
+          <Button onClick={onClose}>CLOSE</Button>
+        </DialogActions>
+        {inProgress && <CircularProgress className={classes.progress} />}
+        <DialogContent>
+          <List>
+            {posts.map(post => (
+              <ReplyPostlistItem key={post.id} post={post} />
+            ))}
+          </List>
+        </DialogContent>
+      </Dialog>
+    )
+  }
+}
+
+const styles = () => ({
+  root: {},
+  posts: {},
+  progress: {
+    display: 'block',
+    marginTop: 80,
+    marginLeft: 'auto',
+    marginRight: 'auto'
+  }
+})
+
+export default withStyles(styles)(PostDetailDialog)
