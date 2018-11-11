01a13327b80a1d33ae094f7c4601660233d832ef
uufish <uufish.io@gmail.com>
2018-11-11 18:28:15 +0700

    update: pages/PageAbout

--- src/pages/PageAbout.js
+++ src/pages/PageAbout.js
@@ -1,8 +1,11 @@
+import Card from '@material-ui/core/Card/Card'
+import CardContent from '@material-ui/core/CardContent/CardContent'
 import createStyles from '@material-ui/core/styles/createStyles'
 import withStyles from '@material-ui/core/styles/withStyles'
-import Info from '@material-ui/icons/Info'
+import Typography from '@material-ui/core/Typography/Typography'
 import React from 'react'
-import { UnderDevelopment } from '../components/UnderDevelopment'
+import { px } from '../libs/styles/px'
+import { resetList } from '../libs/styles/resetList'
 
 class Component extends React.Component<any, any> {
   render() {
@@ -10,13 +13,19 @@ class Component extends React.Component<any, any> {
 
     return (
       <div className={classes.root}>
-        <UnderDevelopment
-          Icon={Info}
-          title={'トリセツ'}
-          description={
-            'このサービスの仕組みや使い方を確認できる機能を開発しています。'
-          }
-        />
+        <Typography variant={'h5'}>About</Typography>
+        <ul className={classes.cards}>
+          <li>
+            <Card>
+              <CardContent>
+                <Typography gutterBottom variant={'h6'} component={'h2'}>
+                  Title
+                </Typography>
+                <Typography component="p">Description</Typography>
+              </CardContent>
+            </Card>
+          </li>
+        </ul>
       </div>
     )
   }
@@ -24,7 +33,20 @@ class Component extends React.Component<any, any> {
 
 const styles = ({ spacing }) =>
   createStyles({
-    root: { paddingTop: spacing.unit * 10 }
+    root: {
+      display: 'grid',
+      gridRowGap: px(spacing.unit * 4),
+      paddingTop: spacing.unit * 4,
+      paddingLeft: spacing.unit * 2,
+      paddingRight: spacing.unit * 2
+    },
+    cards: {
+      ...resetList(),
+      margin: '0 auto',
+      maxWidth: spacing.unit * 100,
+      display: 'grid',
+      gridRowGap: px(spacing.unit * 4)
+    }
   })
 
 export const PageAbout = withStyles(styles)(Component)
