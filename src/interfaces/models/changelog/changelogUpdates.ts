2eff9c4de5fe7a8ff32299c17ec6beeb362f40ab
uufish <uufish.io@gmail.com>
2018-12-01 22:53:39 +0700

    update

--- src/interfaces/models/changelog/changelogUpdates.ts
+++
@@ -1,8 +0,0 @@
-import { firestore } from 'firebase/app'
-import { Update } from '../system/update'
-
-export interface ChangelogUpdates extends Update {
-  text: string
-  date: firestore.Timestamp
-  changelogType: string
-}
