625dcd3af168d9297a0b94a6c1b37f3c467cb573
reiwa <reiwa@outlook.com.vn>
2019-07-14 16:05:55 +0700

    update: node modules

--- src/app/shared/helpers/initializeApp.ts
+++ src/app/shared/helpers/initializeApp.ts
@@ -1,16 +1,18 @@
-import { firestore, initializeApp } from 'firebase/app'
+import { firestore, initializeApp, performance } from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/firestore'
 import 'firebase/functions'
+import 'firebase/performance'
 import 'firebase/storage'
 
 initializeApp({
   apiKey: 'AIzaSyD5ApayvhirvS8kST4l3VyyTwUUWQZXgWU',
   authDomain: 'umfzwkzvrtpe.firebaseapp.com',
   databaseURL: 'https://umfzwkzvrtpe.firebaseio.com',
-  messagingSenderId: '974311466905',
   projectId: 'umfzwkzvrtpe',
-  storageBucket: 'umfzwkzvrtpe.appspot.com'
+  storageBucket: 'umfzwkzvrtpe.appspot.com',
+  messagingSenderId: '974311466905',
+  appId: '1:974311466905:web:c7fc0cc24a15e6cb'
 })
 
 firestore()
@@ -18,3 +20,5 @@ firestore()
   .catch(err => {
     console.error(err)
   })
+
+performance()
