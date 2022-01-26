81c392de5ff68d14802496190913e4c093d850e2
reiwa <reiwa@outlook.com.vn>
2022-01-27 00:11:34 +0900

    update

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  presets: ["blitz/babel"],
-  plugins: [
-    ["babel-plugin-transform-typescript-metadata"],
-    ["@babel/plugin-proposal-decorators", { legacy: true }],
-    ["@babel/plugin-proposal-class-properties", { loose: true }],
-    ["@babel/plugin-proposal-private-property-in-object", { loose: true }],
-  ],
-}
