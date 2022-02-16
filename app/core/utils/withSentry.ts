16a1be7cce027bf72ecd83a0759137529efd458d
reiwa <reiwa@outlook.com.vn>
2022-02-17 01:33:48 +0900

    update

--- app/core/utils/withSentry.ts
+++ app/core/utils/withSentry.ts
@@ -9,29 +9,32 @@ import {
 import "@sentry/tracing"
 import { Ctx } from "blitz"
 import { InternalError } from "integrations/errors"
-import "reflect-metadata"
 
 type Resolver<T, U> = (t: T, ctx: Ctx) => PromiseLike<U>
 
 export const withSentry = <T, U>(resolver: Resolver<T, U>, name: string) => {
-  if (process.env.NODE_ENV !== "development") {
-    init({
-      dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-      tracesSampleRate: 1.0,
-      attachStacktrace: true,
-      normalizeDepth: 5,
-      environment: process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT,
-      integrations: [new Integrations.Http({ tracing: true })],
-      release: process.env.SENTRY_RELEASE,
-      debug: false,
-    })
-  }
-
   return async (props: T, ctx: Ctx) => {
     try {
+      init({
+        dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+        tracesSampleRate: 1.0,
+        attachStacktrace: true,
+        normalizeDepth: 5,
+        environment: process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT,
+        integrations: [new Integrations.Http({ tracing: true })],
+        release: process.env.SENTRY_RELEASE,
+        debug: false,
+        beforeSend(event) {
+          if (process.env.NODE_ENV === "production") {
+            return null
+          }
+          return event
+        },
+      })
+
       setUser({ user: { id: ctx.session?.userId } })
 
-      setContext("input", props)
+      setContext("props", props)
 
       const transaction = startTransaction({ op: "function", name })
 
