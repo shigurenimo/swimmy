ca966845965068b631134c098312f81e471beabc
reiwa <reiwa@outlook.com.vn>
2022-10-29 04:46:17 +0900

    refactor

--- types.ts
+++ types.ts
@@ -1,4 +1,4 @@
-import { SessionContext, SimpleRolesIsAuthorized } from "@blitzjs/auth";
+import { SessionContext, SimpleRolesIsAuthorized } from "@blitzjs/auth"
 import { User } from "db"
 
 // Note: You should switch to Postgres and then use a DB enum for role type
